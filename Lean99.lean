d705dc2401b405be1650e0f65b0004b0907e5476
Yuki Minoh <yukikaze.0511@gmail.com>
2025-11-18 14:10:41 +0000

    small changes

--- Lean99.lean
+++ Lean99.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `Lean99` library.
 -- Import modules here that should be built as part of the library.
-import Lean99.Basic
+-- import Lean99.Basic
--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
 import Lean99
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println s!"Hello, world!"
